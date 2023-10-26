60cec22f54a5a0182a5b339bfc9969d808eaec6d
junowoz <junoguvea@gmail.com>
2023-10-25 23:32:45 -0400

    colaborador comments

--- digiboard-front/src/app/components/colaborador/colaborador.component.ts
+++ digiboard-front/src/app/components/colaborador/colaborador.component.ts
@@ -6,14 +6,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './colaborador.component.html',
 })
 export class ColaboradorComponent {
-  @Input() colaborador: any;
-  @Output() edit = new EventEmitter<any>();
-  @Output() delete = new EventEmitter<number>();
+  @Input() colaborador: any; // Prop. de entrada para receber dados do componente pai.
+  @Output() edit = new EventEmitter<any>(); // Emite os dados do colaborador que será editado.
+  @Output() delete = new EventEmitter<number>(); // Emite o id do colaborador que será excluído.
 
+  // Método chamado quando o botão 'Editar' é clicado. Emite o evento com os dados do colaborador atual.
   onEdit() {
     this.edit.emit(this.colaborador);
   }
 
+  // Método chamado quando o botão 'Excluir' é clicado. Emite o evento com o id do colaborador atual.
   onDelete() {
     this.delete.emit(this.colaborador.id);
   }
